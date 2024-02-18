d50f5c511aa53ab2c76b733bb3a365b1fe727408
aoao271 <140951327+aoao271@users.noreply.github.com>
2024-02-19 00:40:02 +0900

    Create home.js

---
+++ home.js
@@ -0,0 +1,18 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".nav-links li");
+
+burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active")
+
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = "";
+        } else {
+            link.style.animation = `navLinksFade 0.5s ease forwards ${
+                index / 7 + 0.4
+            }s`;
+        }
+    });
+    burger.classList.toggle("toggle");
+});
